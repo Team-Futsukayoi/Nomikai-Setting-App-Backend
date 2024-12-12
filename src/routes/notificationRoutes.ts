5a901d86b03c18fedd33b6df340d485a1e1659a1
Jou Okuyama <ojoxux@gmail.com>
2024-12-12 11:00:47 +0900

    feat: ✨ notificationRoutes モジュールを追加

---
+++ src/routes/notificationRoutes.ts
@@ -0,0 +1,8 @@
+import { Router } from 'express';
+import { notificationController } from '../controllers/notificationController';
+
+const router = Router();
+
+router.post('/send', notificationController.sendNotification);
+
+export const notificationRoutes = router;
